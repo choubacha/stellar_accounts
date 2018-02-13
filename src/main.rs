df9805e316553bffab925df04c6bd479ac5494f8
Kevin Bacha <chewbacha@gmail.com>
2018-02-12 20:59:34 -0800

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,12 @@ struct Asset {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Embedded<T> {
-    _embedded: Records<T>
+    _embedded: Records<T>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Records<T> {
-    records: Vec<T>
+    records: Vec<T>,
 }
 
 enum Command {
@@ -43,13 +43,11 @@ fn get_command() -> Command {
 
     let mut command = String::new();
     match io::stdin().read_line(&mut command) {
-        Ok(n) if n > 1 => {
-            match command.trim() {
-                "1" => Command::ScottsBalance,
-                "2" => Command::MobiInfo,
-                "q" => Command::Quit,
-                _   => Command::TryAgain,
-            }
+        Ok(n) if n > 1 => match command.trim() {
+            "1" => Command::ScottsBalance,
+            "2" => Command::MobiInfo,
+            "q" => Command::Quit,
+            _ => Command::TryAgain,
         },
         Ok(_) => Command::Quit,
         Err(e) => {
@@ -66,29 +64,39 @@ fn main() {
             Command::ScottsBalance => {
                 let url = "https://horizon-testnet.stellar.org";
                 let account_id = "GATUX2IIOOPQ5CWW3SFVOFUKBBSJ72GIGROR3U4PAA7VUYZT5AYD3HL4";
-                let mut response = client.get(&format!("{}/accounts/{}", url, account_id))
+                let mut response = client
+                    .get(&format!("{}/accounts/{}", url, account_id))
                     .send()
                     .unwrap();
                 let json_str = response.text().expect("there was no body");
-                let account: Account = serde_json::from_str::<Account>(&json_str)
-                    .expect("invalid json");
+                let account: Account =
+                    serde_json::from_str::<Account>(&json_str).expect("invalid json");
                 println!("id:          {}", account.id);
                 println!("account id:  {}", account.account_id);
                 println!("sequence:    {}", account.sequence);
-            },
+            }
             Command::MobiInfo => {
                 let url = "https://horizon.stellar.org";
                 let asset_code = "MOBI";
                 let asset_issuer = "GBNMSB7UXFOGWC3E6BQRKYOKJCAYUL6WBZMCINHKGWAD3PTRXNWUQ2BB";
-                let mut response = client.get(&format!("{}/assets?asset_code={}&asset_issuer={}", url, asset_code, asset_issuer))
+                let mut response = client
+                    .get(&format!(
+                        "{}/assets?asset_code={}&asset_issuer={}",
+                        url, asset_code, asset_issuer
+                    ))
                     .send()
                     .unwrap();
                 let json_str = response.text().expect("there was no body");
                 let assets: Vec<Asset> = serde_json::from_str::<Embedded<Asset>>(&json_str)
-                    .expect("invalid json")._embedded.records;
+                    .expect("invalid json")
+                    ._embedded
+                    .records;
                 let asset = &assets[0];
-                println!("{} accounts hold {} issued by {}", asset.num_accounts, asset.asset_code, asset.asset_issuer);
-            },
+                println!(
+                    "{} accounts hold {} issued by {}",
+                    asset.num_accounts, asset.asset_code, asset.asset_issuer
+                );
+            }
             Command::Quit => break,
             _ => continue,
         }
