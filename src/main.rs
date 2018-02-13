19c5321a2e30dffdea90e79c1a8c2183a8f93876
Correlator <scotthoch86@gmail.com>
2018-02-12 20:47:57 -0800

    very clean implementation of asset get

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 
+use std::io;
 use std::io::Read;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -14,17 +15,55 @@ struct Account {
     sequence: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Asset {
+    asset_code: String,
+    asset_issuer: String,
+    num_accounts: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Embedded<T> {
+    _embedded: Records<T>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Records<T> {
+    records: Vec<T>
+}
+
 fn main() {
-    let url = "https://horizon-testnet.stellar.org";
-    let account_id = "GATUX2IIOOPQ5CWW3SFVOFUKBBSJ72GIGROR3U4PAA7VUYZT5AYD3HL4";
     let client = reqwest::Client::new();
-    let mut response = client.get(&format!("{}/accounts/{}", url, account_id))
-        .send()
-        .unwrap();
-    let json_str = response.text().expect("there was no body");
-    let account: Account = serde_json::from_str::<Account>(&json_str)
-        .expect("invalid json");
-    println!("id:          {}", account.id);
-    println!("account id:  {}", account.account_id);
-    println!("sequence:    {}", account.sequence);
+
+    println!("Type 1 to get scotts testnet balance, Type 2 to get some MOBI info");
+    let mut command = String::new();
+    io::stdin().read_line(&mut command)
+        .expect("Did not read input");
+    let command = command.trim();
+    if command == "1" {
+        let url = "https://horizon-testnet.stellar.org";
+        let account_id = "GATUX2IIOOPQ5CWW3SFVOFUKBBSJ72GIGROR3U4PAA7VUYZT5AYD3HL4";
+        let mut response = client.get(&format!("{}/accounts/{}", url, account_id))
+            .send()
+            .unwrap();
+        let json_str = response.text().expect("there was no body");
+        let account: Account = serde_json::from_str::<Account>(&json_str)
+            .expect("invalid json");
+        println!("The id is: {}", account.id);
+    } else if command == "2" {
+        let url = "https://horizon.stellar.org";
+        let asset_code = "MOBI";
+        let asset_issuer = "GBNMSB7UXFOGWC3E6BQRKYOKJCAYUL6WBZMCINHKGWAD3PTRXNWUQ2BB";
+        let mut response = client.get(&format!("{}/assets?asset_code={}&asset_issuer={}", url, asset_code, asset_issuer))
+            .send()
+            .unwrap();
+        let json_str = response.text().expect("there was no body");
+        println!("Mobi is: {}", json_str);
+        let assets: Vec<Asset> = serde_json::from_str::<Embedded<Asset>>(&json_str)
+            .expect("invalid json")._embedded.records;
+        let asset = &assets[0];
+        println!("{} accounts hold {} issued by {}", asset.num_accounts, asset.asset_code, asset.asset_issuer);
+    } else {
+        println!("Don't be silly");
+    }
 }
